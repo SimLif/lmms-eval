2ac6ce5b7d26e2711b80b5efa246940e65a1a714
SimLif <517992857@qq.com>
2025-02-12 16:12:35 +0800

    初步构建vqa_rad并通过测试

---
+++ lmms_eval/tasks/vqa_rad/metrics.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from .glossary import normalize_word
+
+def split_sentence(sentence, n):
+    words = defaultdict(int)
+    # tmp_sentence = re.sub("[^a-zA-Z ]", "", sentence)
+    tmp_sentence = sentence
+    tmp_sentence = tmp_sentence.lower()
+    tmp_sentence = tmp_sentence.strip().split()
+    length = len(tmp_sentence)
+    for i in range(length - n + 1):
+        tmp_words = " ".join(tmp_sentence[i: i + n])
+        if tmp_words:
+            words[tmp_words] += 1
+    return words
+
+
+def calculate_exactmatch(candidate, reference):
+
+    candidate = normalize_word(candidate)
+    reference = normalize_word(reference)
+
+    candidate_words = split_sentence(candidate, 1)
+    reference_words = split_sentence(reference, 1)
+    count = 0
+    total = 0
+    for word in reference_words:
+        if word in candidate_words:
+            count += 1
+    for word in candidate_words:
+        total += candidate_words[word]
+        
+    if total == 0:
+        return 0 # "0 (warning: length of candidate's words is 0)"
+    else:
+        return count / total
