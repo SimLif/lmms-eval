f4c63d914643696b823c4ba8e39aa5c62b71d5e5
SimLif <517992857@qq.com>
2025-04-26 20:55:14 +0800

    update qwen2_vl_moe

--- lmms_eval/models/model_utils/moellava/model/multimodal_model/qwen2_vl_moe.py
+++ lmms_eval/models/model_utils/moellava/model/multimodal_model/qwen2_vl_moe.py
@@ -1632,7 +1632,7 @@ class MoEQwen2VLForConditionalGeneration(Qwen2VLForConditionalGeneration):
                 if (labels is not None) and (not self.config.moe['kd_align']):
                     # print(f"Loss: {loss}, MoE Loss: {sum(moe_losses)}, Total: {loss + moe_loss}")
                     loss += moe_loss
-                if self.config.moe['kd_align']:
+                if self.config.moe.get('kd_align', False):
                     loss = moe_loss + 0 * loss
 
         if not return_dict:
