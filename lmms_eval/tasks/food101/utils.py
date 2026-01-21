b3b304526e48906ae65895fdc3cccd5ac3b458a9
SimLif <hguoau@connect.ust.hk>
2026-01-21 20:59:10 +0800

    feat: add food101 dataset

---
+++ lmms_eval/tasks/food101/utils.py
@@ -0,0 +1,17 @@
+def food101_doc_to_visual(doc):
+    return [doc["image"].convert("RGB")]
+
+def food101_doc_to_text(doc, lmms_eval_specific_kwargs=None):
+    question = doc["input"].strip()
+    return question
+
+
+def food101_process_results(doc, results):
+    pred = results[0]
+    pred_ans = pred.lower().strip().replace(".", "")
+    gt_ans = doc["label"].lower().strip().replace(".", "")
+
+    if gt_ans not in pred_ans:
+        return {"accuracy": 0}
+    else:
+        return {"accuracy": 100}
